47a771a96b813f124ab25751961a1c87f55d0536
Happer World <happer64bit@gmail.com>
2023-06-17 13:31:51 +0630

    Finally

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md") as file:
+    long_description = file.read()
 
 setup(
    name='ginz',
    version='1.0',
-   description='Ginz is a command-line tool for cloning multiple repositories from GitHub based on a configuration file. It simplifies the process of cloning multiple repositories by allowing you to specify the repositories and their branches in a single configuration file.',
+   license="MIT",
+   description='Ginz is a command-line utility that simplifies the process of cloning multiple repositories from GitHub by allowing you to specify the repositories and their branches in a single configuration file.',
    author='Happer64Bit',
    author_email='happer64bit@gmail.com',
-   packages=['toml', "git", "colorama"],
-   install_requires=['wheel', 'bar', 'greek'],
+   long_description=long_description,
+   install_requires=["toml", "python-git", "colorama"],
+   package_directory="ginz",
 )
