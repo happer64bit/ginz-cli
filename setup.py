25386441063f1b616eb0bb0e365a79cc2bd6a57d
Happer World <happer64bit@gmail.com>
2023-06-18 11:02:35 +0630

    request package missing

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     author_email='happer64bit@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["toml", "GitPython", "colorama", "tqdm", "request"],
+    install_requires=["toml", "GitPython", "colorama", "tqdm", "requests"],
     package_directory="ginz",
 )
